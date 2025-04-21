37456b30b0eeb2e80a867d16b461cbca367deba9
kaeky <carlosbasan2011@hotmail.com>
2025-04-20 23:04:33 -0500

    feat: create base structure project

--- BackEnd/src/app.module.ts
+++ BackEnd/src/app.module.ts
@@ -5,6 +5,12 @@ import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { typeOrmConfig } from './config/database.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DepartmentsModule } from './departments/departments.module';
+import { CitiesModule } from './cities/cities.module';
+import { ProductsModule } from './products/products.module';
+import { CompaniesModule } from './companies/companies.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,6 +19,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       load: [configuration],
     }),
     TypeOrmModule.forRoot(typeOrmConfig),
+    DepartmentsModule,
+    CitiesModule,
+    ProductsModule,
+    CompaniesModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
